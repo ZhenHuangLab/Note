2c1e6d311ba56adc49db7aa0f936ebd3ddc49936
Zhen Huang <zhen.victor.huang@gmail.com>
2025-09-28 00:14:16 +0800

    chore: ignore .docusaurus and build folder

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,9 +0,0 @@
-import lunr from "/Users/zhenhuang/Documents/Note/node_modules/lunr/lunr.js";
-require("/Users/zhenhuang/Documents/Note/node_modules/lunr-languages/lunr.stemmer.support.js")(lunr);
-require("/Users/zhenhuang/Documents/Note/node_modules/@easyops-cn/docusaurus-search-local/dist/client/shared/lunrLanguageZh.js").lunrLanguageZh(lunr);
-require("/Users/zhenhuang/Documents/Note/node_modules/lunr-languages/lunr.multi.js")(lunr);
-export const removeDefaultStopWordFilter = [];
-export const language = ["en","zh"];
-export const searchIndexUrl = "search-index{dir}.json?_=aa875cd4";
-export const searchResultLimits = 8;
-export const fuzzyMatchingDistance = 1;
--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/zhenhuang/Documents/Note/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/zhenhuang/Documents/Note/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/zhenhuang/Documents/Note/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/zhenhuang/Documents/Note/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/zhenhuang/Documents/Note/src/css/custom.css"),
-];
