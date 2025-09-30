82df35c4917a044510fecda6b86f568ff49f7357
Zhen Huang <zhen.victor.huang@gmail.com>
2025-09-30 15:53:28 +0800

    fix: update indexName of the crawler

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -182,16 +182,16 @@ const config: Config = {
       appId: 'PS8L8YQZAP',
       // Public API key: it is safe to commit it
       apiKey: '23d5b8ea4f6ec77f39240b2100ede82b',
-      indexName: "Zhen Huang's Note",
+      indexName: "note.zhenhuang.top",
       // Optional: see doc section below
       contextualSearch: true,
       // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
       // externalUrlRegex: 'external\\.com|domain\\.com',
       // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      // replaceSearchResultPathname: {
-      //   from: '/docs/', // or as RegExp: /\/docs\//
-      //   to: '/',
-      // },
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
 
       // Optional: Algolia search parameters
       // searchParameters: {},
