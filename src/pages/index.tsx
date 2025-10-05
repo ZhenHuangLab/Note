bff35e205a176a61d57e85e852e83c7f1d61dfd9
Zhen Huang <zhen.victor.huang@gmail.com>
2025-10-05 10:20:27 +0800

    chore: remove footer for the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,11 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import Head from '@docusaurus/Head';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HomepageCard from '@site/src/components/Cards/HomepageCard';
-import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
@@ -39,19 +14,18 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+      noFooter>
       <Head>
         <link rel="stylesheet" href={cardsBaseCss} />
         <link rel="stylesheet" href={cardsCss} />
       </Head>
-      <HomepageHeader />
       <main>
         <section className={styles.cardShowcase}>
           <div className={styles.cardContainer}>
             <HomepageCard />
           </div>
         </section>
-        <HomepageFeatures />
       </main>
     </Layout>
   );
